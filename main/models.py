3f39bb914f6405cc397e82d524510d177dbe9263
mirgul81 <bekzbeks9@gmail.com>
2023-01-21 16:47:15 +0600

    html fayly

--- main/models.py
+++ main/models.py
@@ -1,28 +1,28 @@
-from django.db import models
-
-
-class News(models.Model):
-    title = models.CharField(max_length=60)
-    info = models.TextField()
-
-    def __str__(self):
-        return self.title
-
-class Students(models.Model):
-        name = models.CharField(max_length=50)
-        surname = models.CharField(max_length=50)
-        age = models.PositiveIntegerField()
-        faculty = models.CharField(max_length=50)
-
-        def __str__(self):
-            return self.name
-
-
-class Abouts(models.Model):
-    title = models.CharField(max_length=60)
-    info = models.TextField()
-
-    def __str__(self):
-        return self.title
-
-
+# from django.db import models
+#
+#
+# class News(models.Model):
+#     title = models.CharField(max_length=60)
+#     info = models.TextField()
+#
+#     def __str__(self):
+#         return self.title
+#
+# class Students(models.Model):
+#         name = models.CharField(max_length=50)
+#         surname = models.CharField(max_length=50)
+#         age = models.PositiveIntegerField()
+#         faculty = models.CharField(max_length=50)
+#
+#         def __str__(self):
+#             return self.name
+#
+#
+# class Abouts(models.Model):
+#     title = models.CharField(max_length=60)
+#     info = models.TextField()
+#
+#     def __str__(self):
+#         return self.title
+#
+#
